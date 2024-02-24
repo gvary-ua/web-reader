aaf7ec2888f1e4581bb76ab5f66753b660376483
s1ckret <ivan.kovalenko@s1ckret.com>
2024-02-24 19:01:32 +0200

    feat(#4): add covers table
    Signed-off-by: s1ckret <ivan.kovalenko@s1ckret.com>

---
+++ app/Models/Cover.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Cover extends Model
+{
+    use HasFactory;
+    protected $table = 'covers';
+
+    protected $primaryKey = 'cover_id';
+
+    protected $fillable = [
+        'cover_type_id',
+        'status_id',
+        'lang_id',
+        'title',
+        'description',
+        'img_key',
+        'public',
+        'average_reading_time_sec',
+        'words_count',
+        'published_at',
+        'finished_at'
+    ];
+
+    protected $casts = [
+        'published_at' => 'datetime',
+        'finished_at' => 'datetime',
+    ];
+    
+}
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CoverController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,6 +22,8 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::get('/covers/{cover_id}', [CoverController::class, 'show'])->name('covers.show');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
