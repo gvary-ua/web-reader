2d8bc7335e262fba9ac7ad1099fefb4afef3b931
s1ckret <ivan.kovalenko@s1ckret.com>
2024-03-02 12:25:41 +0200

    docs(#11): add scribe documentation for blocks API
    Signed-off-by: s1ckret <ivan.kovalenko@s1ckret.com>

--- app/Http/Controllers/Api/V1/BlocksApi.php
+++ app/Http/Controllers/Api/V1/BlocksApi.php
@@ -11,10 +11,55 @@ use Illuminate\Support\Facades\DB;
 use App\Models\Chapter;
 use Illuminate\Support\Arr;
 
+/**
+ * @group Blocks APIs
+ * 
+ * APIs for creating and getting chapters.
+ * 
+ * @authenticated
+ */
 class BlocksApi extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * Retrieve all blocks associated with a specific chapter.
+     * 
+     * @pathParam chapterId integer required The ID of the chapter this block is associated with.
+     * @response 200 {
+     *     "data": [
+     *         {
+     *             "id": "xlb4JE-i_B",
+     *             "typeId": 1,
+     *             "data": {
+     *                 "text": "Hello world!",
+     *                 "version": "1.0.0"
+     *             }
+     *         },
+     *         {
+     *             "id": "Q3H8zb0Mq6",
+     *             "typeId": 1,
+     *             "data": {
+     *                 "text": "Hello!",
+     *                 "version": "1.0.0"
+     *             }
+     *         },
+     *         {
+     *             "id": "md4023qhRg",
+     *             "typeId": 1,
+     *             "data": {
+     *                 "text": "yes yes yes yes",
+     *                 "version": "1.0.0"
+     *             }
+     *         },
+     *         {
+     *             "id": "ThmRYxu7LT",
+     *             "typeId": 1,
+     *             "data": {
+     *                 "text": "nice beer realy",
+     *                 "version": "1.0.0"
+     *             }
+     *         }
+     *     ]
+     * }
      */
     public function index(IndexBlocksRequest $request)
     {
@@ -29,7 +74,7 @@ class BlocksApi extends Controller
             ->get();
 
         // Decode the JSON string into JSON object
-        $blocks = array_map(function($block) {
+        $blocks = array_map(function ($block) {
             $block->data = json_decode($block->data);
             return $block;
         }, $blocks->all());
@@ -38,17 +83,21 @@ class BlocksApi extends Controller
     }
 
     /**
-     * Store a newly created resource in storage.
+     * Store multiple blocks for a specific chapter.
+     * 
+     * @pathParam chapterId integer required The ID of the chapter this block is associated with.
+     * @response 200
+     * @response 422 {"message":"The blocks.0.data.version field is required. (and 3 more errors)","errors":{"blocks.0.data.version":["The blocks.0.data.version field is required."],"blocks.1.data.version":["The blocks.1.data.version field is required."],"blocks.2.data.version":["The blocks.2.data.version field is required."],"blocks.3.data.version":["The blocks.3.data.version field is required."]}}
      */
     public function bulkStore(BulkStoreBlocksRequests $request)
     {
         $chapterId = $request->chapterId;
 
-        $block_ids = collect($request->blocks)->map(function($block) {
+        $block_ids = collect($request->blocks)->map(function ($block) {
             return $block['block_nanoid_10'];
         })->toArray();
-        
-        $blocks = collect($request->blocks)->map(function($block) {
+
+        $blocks = collect($request->blocks)->map(function ($block) {
             return Arr::except($block, ['id', 'typeId', 'wordCount']);
         })->toArray();
 
