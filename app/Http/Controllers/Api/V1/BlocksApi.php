f2dad46442de263da00f9a3d5d84ee495f4e600b
s1ckret <ivan.kovalenko@s1ckret.com>
2024-03-02 12:13:11 +0200

    feat(#11): implement GET blocks API
    Signed-off-by: s1ckret <ivan.kovalenko@s1ckret.com>

--- app/Http/Controllers/Api/V1/BlocksApi.php
+++ app/Http/Controllers/Api/V1/BlocksApi.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\V1\BulkStoreBlocksRequests;
+use App\Http\Requests\Api\V1\IndexBlocksRequest;
+use App\Http\Resources\V1\BlockResource;
 use App\Models\Block;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
@@ -15,9 +17,25 @@ class BlocksApi extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(IndexBlocksRequest $request)
     {
-        //
+        $chapterId = $request->chapterId;
+
+        $block_ids = Chapter::where('chapter_id', $chapterId)
+            ->first()
+            ->block_ids;
+
+        $blocks = Block::whereIn('block_nanoid_10', $block_ids)
+            ->orderByRaw('FIELD(block_nanoid_10, "' . implode('","', $block_ids) . '")')
+            ->get();
+
+        // Decode the JSON string into JSON object
+        $blocks = array_map(function($block) {
+            $block->data = json_decode($block->data);
+            return $block;
+        }, $blocks->all());
+
+        return BlockResource::collection($blocks);
     }
 
     /**
