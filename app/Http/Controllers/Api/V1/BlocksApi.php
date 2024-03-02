a0cb826049eb7f1812316a5a037f0d59d3f84eeb
s1ckret <ivan.kovalenko@s1ckret.com>
2024-03-02 12:18:23 +0200

    chore(#11): remove empty methods
    Signed-off-by: s1ckret <ivan.kovalenko@s1ckret.com>

--- app/Http/Controllers/Api/V1/BlocksApi.php
+++ app/Http/Controllers/Api/V1/BlocksApi.php
@@ -7,7 +7,6 @@ use App\Http\Requests\Api\V1\BulkStoreBlocksRequests;
 use App\Http\Requests\Api\V1\IndexBlocksRequest;
 use App\Http\Resources\V1\BlockResource;
 use App\Models\Block;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\Models\Chapter;
 use Illuminate\Support\Arr;
@@ -41,14 +40,6 @@ class BlocksApi extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
     public function bulkStore(BulkStoreBlocksRequests $request)
     {
         $chapterId = $request->chapterId;
@@ -66,29 +57,5 @@ class BlocksApi extends Controller
                 ->update(['block_ids' => $block_ids]);
             Block::upsert($blocks, ['block_nanoid_10'], ['block_type_id', 'data', 'word_count']);
         });
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(Block $block)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, Block $block)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Block $block)
-    {
-        //
     }
 }
