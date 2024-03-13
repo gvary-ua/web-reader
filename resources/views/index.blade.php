0bdcc2ad1902df9d742ab780263da84e3182ec43
s1ckret <ivan.kovalenko@s1ckret.com>
2024-03-13 21:48:18 +0200

    chore(#29): delete generated templates
    Signed-off-by: s1ckret <ivan.kovalenko@s1ckret.com>

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,11 +1,3 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-  <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-
-    <title>Laravel</title>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
-  </head>
-  <body></body>
-</html>
+<x-app-layout>
+  <x-button variant="secondary-1" size="2xl">Button</x-button>
+</x-app-layout>
