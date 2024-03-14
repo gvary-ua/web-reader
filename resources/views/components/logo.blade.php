18c1a2f27f611d1ab0fe4631a761a81578c4a735
s1ckret <ivan.kovalenko@s1ckret.com>
2024-03-14 18:13:42 +0200

    feat(#29): add logo component
    Signed-off-by: s1ckret <ivan.kovalenko@s1ckret.com>

---
+++ resources/views/components/logo.blade.php
@@ -0,0 +1,5 @@
+@if ($attributes->has('withText'))
+  <img {{ $attributes }} src="/icons/logo-text.svg" alt="Logo svg" />
+@else
+  <img {{ $attributes }} src="/icons/logo.svg" alt="Logo svg" />
+@endif
