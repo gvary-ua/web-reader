27a6fb2b8e1453c189b8fc6d493ee9b0889be354
s1ckret <ivan.kovalenko@s1ckret.com>
2024-03-15 12:37:35 +0200

    feat(#29): implement landing page
    Signed-off-by: s1ckret <ivan.kovalenko@s1ckret.com>

---
+++ resources/views/components/gvary-comment.blade.php
@@ -0,0 +1,11 @@
+<div {{ $attributes }}>
+  <div class="relative text-center">
+    <img src="/icons/comment-landing.svg" alt="Comment" class="ml-auto mr-[12%]" />
+    <x-p size="xs" class="absolute right-[calc(12%+25px)] top-[22px]">
+      Гортай нижче,
+      <br />
+      підібрали для тебе найкраще!
+    </x-p>
+  </div>
+  <x-logo class="ml-auto mr-[5%]" />
+</div>
---
+++ resources/views/components/section/hero.blade.php
@@ -0,0 +1,27 @@
+<section
+  {{
+    $attributes->merge([
+      'class' => 'relative bg-secondary-1 text-on-secondary-1 ',
+    ])
+  }}
+>
+  <x-h level="h1">Спільнота письменників</x-h>
+  <x-h level="h2" class="font-bold">та читачів</x-h>
+  <x-p size="2xl" class="mt-6 sm:mt-10">
+    Занурюйся у всесвіт,
+    <br class="sm:hidden" />
+    де ти можеш писати та
+    <br class="sm:hidden" />
+    обговорювати
+    <br class="hidden md:inline lg:hidden" />
+    найкращі
+    <br class="sm:hidden" />
+    новели
+    <br class="hidden lg:inline" />
+    та романи, які варті
+    <br class="sm:hidden" />
+    твоєї уваги
+  </x-p>
+  <x-button variant="primary" size="2xl" class="mt-12 w-full sm:w-fit">Доєднатися</x-button>
+  <x-gvary-comment class="mb-[-5rem] mt-8" />
+</section>
