b5bde40719e616f297b9d7ccdebe4a38cd67b778
s1ckret <ivan.kovalenko@s1ckret.com>
2024-03-15 12:37:35 +0200

    feat(#29): implement landing page
    Signed-off-by: s1ckret <ivan.kovalenko@s1ckret.com>

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,3 +1,5 @@
 <x-app-layout>
-  <x-button variant="secondary-1" size="2xl">Button</x-button>
+  <x-section.hero class="px-4 py-10 sm:px-10 md:px-14 lg:px-16" />
+
+  <x-h level="h3" class="mt-20 px-4 py-10 text-center sm:px-10 md:px-14 lg:px-16">Скоро буде більше!</x-h>
 </x-app-layout>
