ee22e7135f981cf1cb81fd98db3ed5aaf64eb8f9
s1ckret <ivan.kovalenko@s1ckret.com>
2024-03-15 10:44:24 +0200

    feat(#29): add h component
    Signed-off-by: s1ckret <ivan.kovalenko@s1ckret.com>

---
+++ resources/views/components/h.blade.php
@@ -0,0 +1,28 @@
+@props([
+  'level' => 'h1',
+])
+
+@php
+  switch ($level) {
+    case 'h1':
+    default:
+      $levelClasses = 'font-roslindaleCyrillic text-[2.5rem] leading-tight sm:text-[4.5rem] sm:leading-[0.825] md:text-[6rem] lg:text-[8rem]';
+      break;
+    case 'h2':
+      $levelClasses = 'font-robotoFlex text-[2.5rem] leading-tight sm:text-[4.5rem] sm:leading-[0.825] md:text-[6rem] lg:text-[8rem]';
+      break;
+    case 'h3':
+      $levelClasses = 'font-roslindaleCirillic text-[3rem] leading-none';
+      break;
+    case 'h4':
+      $levelClasses = 'font-robotoFlex text-[2rem] leading-none';
+      break;
+    case 'h5':
+      $levelClasses = 'font-roslindaleCirillic text-base leading-none';
+      break;
+  }
+@endphp
+
+<{{ $level }} {{ $attributes->merge(['class' => implode(' ', [$levelClasses])]) }}>
+  {{ $slot }}
+</{{ $level }}>
