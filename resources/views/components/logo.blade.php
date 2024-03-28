b8eac47be1a1dc829d53e1e7f63605df0799ee0e
s1ckret <ivan.kovalenko@s1ckret.com>
2024-03-28 23:38:03 +0200

    feat(31): add href to logo
    Signed-off-by: s1ckret <ivan.kovalenko@s1ckret.com>

--- resources/views/components/logo.blade.php
+++ resources/views/components/logo.blade.php
@@ -1,5 +1,7 @@
-@if ($attributes->has('withText'))
-  <img {{ $attributes }} src="/icons/logo-text.svg" alt="Logo svg" />
-@else
-  <img {{ $attributes }} src="/icons/logo.svg" alt="Logo svg" />
-@endif
+<a class="inline-block" href="/">
+  @if ($attributes->has('withText'))
+    <img {{ $attributes }} src="/icons/logo-text.svg" alt="Logo svg" />
+  @else
+    <img {{ $attributes }} src="/icons/logo.svg" alt="Logo svg" />
+  @endif
+</a>
