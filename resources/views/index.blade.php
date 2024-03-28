345a5d1bfb7f75a3427fc499d5bb36c3085fc06c
s1ckret <ivan.kovalenko@s1ckret.com>
2024-03-28 23:38:32 +0200

    chore(31): remove temporary logout
    Signed-off-by: s1ckret <ivan.kovalenko@s1ckret.com>

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,12 +2,4 @@
   <x-section.hero class="px-4 py-10 sm:px-10 md:px-14 lg:px-16" />
 
   <x-h level="h3" class="mt-20 px-4 py-10 text-center sm:px-10 md:px-14 lg:px-16">Скоро буде більше!</x-h>
-
-  <form method="POST" action="{{ route('logout') }}">
-    @csrf
-
-    <x-button type="submit" variant="secondary" size="base">
-      {{ __('Log Out') }}
-    </x-button>
-  </form>
 </x-app-layout>
