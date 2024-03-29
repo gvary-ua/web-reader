0967cc1badbdc305c56686ad42e13e85d6c7470a
s1ckret <ivan.kovalenko@s1ckret.com>
2024-03-29 11:20:25 +0200

    feat(31): add about page template
    Signed-off-by: s1ckret <ivan.kovalenko@s1ckret.com>

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,10 @@ Route::get('/', function () {
     return view('index');
 });
 
+Route::get('/about', function () {
+    return view('about');
+});
+
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::resource('profile', ProfileController::class);
 });
