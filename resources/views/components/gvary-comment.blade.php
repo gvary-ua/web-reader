610144e5f202233174de4fece50d81b6ee331300
s1ckret <ivan.kovalenko@s1ckret.com>
2024-04-11 17:35:00 +0300

    fix(40): add more stable position for gvary-comment
    Signed-off-by: s1ckret <ivan.kovalenko@s1ckret.com>

--- resources/views/components/gvary-comment.blade.php
+++ resources/views/components/gvary-comment.blade.php
@@ -1,11 +1,16 @@
-<div {{ $attributes }}>
-  <div class="relative text-center">
-    <img src="/icons/comment-landing.svg" alt="Comment" class="ml-auto mr-[12%]" />
-    <x-p size="xs" class="absolute right-[calc(12%+25px)] top-[22px]">
+<div {{
+  $attributes->merge([
+    'class' => 'relative text-center',
+  ])
+}}>
+  <div class="relative inline-block sm:left-1/4">
+    <img src="/icons/comment-landing.svg" alt="Comment" />
+    <x-p size="xs" class="absolute left-0 top-0 w-full translate-y-full">
       Гортай нижче,
       <br />
       підібрали для тебе найкраще!
     </x-p>
+    <!-- Magic number. Questions? -->
+    <img class="relative left-[87%]" src="/icons/logo.svg" alt="Logo svg" />
   </div>
-  <img class="ml-auto mr-[5%]" src="/icons/logo.svg" alt="Logo svg" />
 </div>
