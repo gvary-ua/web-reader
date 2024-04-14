89b77bc9882bcfdb55dc8cb46177553c664c7ce5
s1ckret <ivan.kovalenko@s1ckret.com>
2024-04-14 22:10:01 +0300

    feat(40): add route protection to profile editing
    Signed-off-by: s1ckret <ivan.kovalenko@s1ckret.com>

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -3,52 +3,55 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\ProfileUpdateRequest;
+use App\Models\User;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\View\View;
 
 class ProfileController extends Controller
 {
-    /**
-     * Show the user's profile.
-     */
     public function index(Request $request): View
     {
-        return view('profile.index', [
-            'user' => $request->user(),
-        ]);
+        return view('profile.index', ['user' => $request->user()]);
     }
 
-    public function editProfile(Request $request): View
+    public function show(User $user): View
     {
-        return view('settings.profile', [
-            'user' => $request->user(),
-        ]);
+        return view('profile.index', ['user' => $user]);
     }
 
-    public function editAccount(Request $request): View
+    public function editProfile(Request $request, User $user): View
     {
-        return view('settings.account', [
-            'user' => $request->user(),
-        ]);
+        Gate::authorize('update', $user);
+
+        return view('settings.profile', ['user' => $user]);
     }
 
-    public function editSecurity(Request $request): View
+    public function editAccount(Request $request, User $user): View
     {
-        return view('settings.security', [
-            'user' => $request->user(),
-        ]);
+        Gate::authorize('update', $user);
+
+        return view('settings.account', ['user' => $user]);
     }
 
-    public function updateProfile(ProfileUpdateRequest $request): RedirectResponse
+    public function editSecurity(Request $request, User $user): View
     {
-        $request->user()->fill($request->validated());
+        Gate::authorize('update', $user);
 
-        $request->user()->save();
+        return view('settings.security', ['user' => $user]);
+    }
 
-        return Redirect::route('settings.profile', ['user' => $request->user()->user_id])
+    public function updateProfile(ProfileUpdateRequest $request, User $user): RedirectResponse
+    {
+        Gate::authorize('update', $user);
+
+        $user->fill($request->validated());
+        $user->save();
+
+        return Redirect::route('settings.profile', ['user' => $user])
             ->with('status', 'Profile updated!');
     }
 
