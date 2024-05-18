13978b08a4cf6835e2c399af901ba4a08ab3553f
s1ckret <ivan.kovalenko@s1ckret.com>
2024-05-18 18:16:06 +0300

    fix(45): fix header font size
    Signed-off-by: s1ckret <ivan.kovalenko@s1ckret.com>

--- resources/views/components/h.blade.php
+++ resources/views/components/h.blade.php
@@ -12,13 +12,13 @@
       $levelClasses = 'font-robotoFlex text-[2.5rem] leading-tight sm:text-[4.5rem] sm:leading-[0.825] md:text-[6rem] lg:text-[8rem]';
       break;
     case 'h3':
-      $levelClasses = 'font-roslindaleCyrillic text-[3rem] leading-none';
+      $levelClasses = 'font-roslindaleCyrillic text-[2.5rem] leading-[1.2]';
       break;
     case 'h4':
-      $levelClasses = 'font-robotoFlex text-[2rem] leading-none';
+      $levelClasses = 'font-robotoFlex text-[2rem] leading-[1.2]';
       break;
     case 'h5':
-      $levelClasses = 'font-roslindaleCyrillic text-[1.125rem] leading-none';
+      $levelClasses = 'font-roslindaleCyrillic text-[1.125rem] leading-[1.5]';
       break;
   }
 @endphp
