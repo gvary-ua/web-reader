3a396640a9ef4968c04064887f198beba90bb063
s1ckret <ivan.kovalenko@s1ckret.com>
2024-06-02 10:02:56 +0300

    fix(#38): clearfix float
    Signed-off-by: s1ckret <ivan.kovalenko@s1ckret.com>

--- resources/views/components/settings/layout.blade.php
+++ resources/views/components/settings/layout.blade.php
@@ -2,7 +2,9 @@
   'user',
 ])
 
-<x-settings.nav :user="$user" class="px-4 pt-8 md:float-left md:ml-10 md:px-0 md:py-10 lg:ml-20"></x-settings.nav>
-<section class="px-4 py-8 md:float-left md:ml-9 md:px-0 md:py-10">
-  {{ $slot }}
+<section class="after:invisible after:clear-both after:block after:leading-[0] after:content-['.']">
+  <x-settings.nav :user="$user" class="px-4 pt-8 md:float-left md:ml-10 md:px-0 md:py-10 lg:ml-20"></x-settings.nav>
+  <div class="after:h-0; px-4 py-8 md:float-left md:ml-9 md:px-0 md:py-10">
+    {{ $slot }}
+  </div>
 </section>
