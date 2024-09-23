e7744ae7fc32d7da4ea7c90a1c4bcfdfc79daa83
s1ckret <ivan.kovalenko@s1ckret.com>
2024-09-23 12:59:40 +0300

    fix(#96): fix checkbox margin
    Signed-off-by: s1ckret <ivan.kovalenko@s1ckret.com>

--- resources/views/components/checkbox-holder.blade.php
+++ resources/views/components/checkbox-holder.blade.php
@@ -2,11 +2,12 @@
   'name',
   'label' => '',
   'items' => [],
+  'innerClass' => '',
 ])
 
 <div {{ $attributes }}>
   <x-p class="mb-2" size="lg">{{ __($label) }}</x-p>
-  <div class="max-h-full overflow-y-auto">
+  <div class="{{ $innerClass }} overflow-y-auto">
     @foreach ($items as $item)
       <x-checkbox :name="$name" :label="$item['label']" :value="$item['value']" :checked="$item['checked']" />
     @endforeach
