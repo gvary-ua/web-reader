deae03e20f3ccb9a252a145b8dd190c7739969da
s1ckret <ivan.kovalenko@s1ckret.com>
2024-11-30 11:51:31 +0200

    build(#72): add js css to vite
    Signed-off-by: s1ckret <ivan.kovalenko@s1ckret.com>

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,13 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
   plugins: [
     laravel({
-      input: ['resources/css/app.css', 'resources/js/app.js', 'resources/js/slider.js'],
+      input: [
+        'resources/css/app.css',
+        'resources/css/search.css',
+        'resources/js/app.js',
+        'resources/js/slider.js',
+        'resources/js/search.js',
+      ],
       refresh: true,
     }),
   ],
