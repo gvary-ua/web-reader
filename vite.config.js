ee7bde821433b3a4bf18dabb2a84b90bb0c837b8
s1ckret <ivan.kovalenko@s1ckret.com>
2024-12-09 11:59:54 +0200

    feat(#58): implement cropping for profile avatar
    Signed-off-by: s1ckret <ivan.kovalenko@s1ckret.com>

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
         'resources/js/app.js',
         'resources/js/slider.js',
         'resources/js/search.js',
+        'resources/js/cropper.js',
       ],
       refresh: true,
     }),
