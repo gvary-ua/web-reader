9dd8df150788c18d5f1a3f0e71185dc7d9427daa
s1ckret <ivan.kovalenko@s1ckret.com>
2024-12-10 14:10:57 +0200

    feat(#59): tidy up footer
    Signed-off-by: s1ckret <ivan.kovalenko@s1ckret.com>

--- resources/views/components/section/footer.blade.php
+++ resources/views/components/section/footer.blade.php
@@ -1,49 +1,28 @@
 <footer class="bg-surface-1 px-4 py-14 sm:px-20 sm:pb-9 sm:pt-16">
   <a class="h-full" href="{{ url('/') }}"><x-logo class="h-full" withText /></a>
   <div class="mt-12 flex flex-wrap justify-between gap-12 sm:mt-8">
-    <ul class="space-y-[10px]">
+    <ul class="max-w-80 space-y-[10px]">
       <li>
-        <x-p size="sm"><a href="/">{{ __('Site rules') }}</a></x-p>
+        <x-p size="sm">{{ __('Gvary is the community of writers and readers') }}.</x-p>
       </li>
       <li>
-        <x-p size="sm"><a href="/">{{ __('User agreement') }}</a></x-p>
+        <x-p size="sm"><a href="{{ route('explore') }}">{{ __('Discover') }}</a></x-p>
       </li>
       <li>
-        <x-p size="sm"><a href="/">{{ __('Public offer agreement') }}</a></x-p>
-      </li>
-      <li>
-        <x-p size="sm"><a href="/">{{ __('Advertise with us') }}</a></x-p>
+        <x-p size="sm"><a href="/about">{{ __('About us') }}</a></x-p>
       </li>
     </ul>
-    <ul class="space-y-[10px]">
+    <ul class="max-w-80 space-y-[10px]">
       <li>
-        <x-p size="sm"><a href="/">{{ __('About us') }}</a></x-p>
+        <x-p size="sm">{{ __('If you need help or feature request') }}:</x-p>
+        <x-p size="sm">help.gvary@s1ckret.com</x-p>
       </li>
       <li>
-        <x-p size="sm"><a href="/">{{ __('I\'m inspired') }}</a></x-p>
-      </li>
-      <li>
-        <x-p size="sm"><a href="/">{{ __('I create') }}</a></x-p>
-      </li>
-      <li>
-        <x-p size="sm"><a href="/">{{ __('Payment') }}</a></x-p>
+        <x-p size="sm">{{ __('Report a bug') }}:</x-p>
+        <x-p size="sm">bug.gvary@s1ckret.com</x-p>
       </li>
     </ul>
-    <ul class="space-y-[10px]">
-      <li>
-        <x-p size="sm"><a href="/">{{ __('contact@gvary.com') }}</a></x-p>
-      </li>
-      <li>
-        <x-p size="sm"><a href="/">{{ __('Facebook') }}</a></x-p>
-      </li>
-      <li>
-        <x-p size="sm"><a href="/">{{ __('Instagram') }}</a></x-p>
-      </li>
-      <li>
-        <x-p size="sm"><a href="/">{{ __('TikTok') }}</a></x-p>
-      </li>
-    </ul>
-    <ul class="space-y-[10px] text-warning">
+    <ul class="max-w-80 space-y-[10px] text-warning">
       <li>
         <x-p size="sm">{{ __('The copyright on works belongs to the authors and is protected by law') }}</x-p>
       </li>
@@ -55,7 +34,7 @@
     </ul>
   </div>
   <x-p class="mt-20 sm:mt-14" size="sm">
-    <a href="/">{{ __('Privacy Policy') }}</a>
+    <a href="/">{{ __('Privacy Policy') }} {{ __('Gvary') }}</a>
     &#169;2024
   </x-p>
 </footer>
